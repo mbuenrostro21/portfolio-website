9fde9b3eaf1b35e1521d510a18983c39250e08dc
mbuenrostro21 <buenrostro1195@gmail.com>
2025-06-29 11:45:58 -0700

    created the NavBar

---
+++ components/NavBar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const NavBar: React.FC = () => {
+  return (
+    <nav className="bg-gray-800 h-12 rounded">
+      <div>
+        <div>
+            
+        </div>
+      </div>
+    </nav>
+  );
+};
